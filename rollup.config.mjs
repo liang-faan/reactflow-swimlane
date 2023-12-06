fd8a94e6a33427a0d9bfc259aaec98cc0faaa8da
liang-faan <liang.faan@gmail.com>
2023-12-06 16:44:39 +0800

    Fix local start up issue

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -10,7 +10,7 @@ import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
-    input: "./src/index.ts",
+    input: "./src/swimlane_module.ts",
     output: [
       // {
       //   file: packageJson.main,
--- src/swimlane_module.ts
+++ src/swimlane_module.ts
@@ -1,2 +1,4 @@
 export * from "./layout";
 export * from "./include";
+export * from "./types";
+export * from "./utils";
